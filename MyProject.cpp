e405b5af6278d8603b78ea30e6b7112626e75881
marce <marcello.desalvo@mail.polimi.it>
2022-07-01 19:14:09 +0200

    upload texture

--- MyProject.cpp
+++ MyProject.cpp
@@ -2,8 +2,8 @@
 
 #include "MyProject.hpp"
 
-const std::string MODEL_PATH = "models/viking_room.obj";
-const std::string TEXTURE_PATH = "textures/viking_room.png";
+const std::string MODEL_PATH = "models/museumTri.obj";
+const std::string TEXTURE_PATH = "textures/parquet.jpg";
 
 // The uniform buffer object used in this example
 struct UniformBufferObject {
@@ -32,8 +32,8 @@ class MyProject : public BaseProject {
 	// Here you set the main application parameters
 	void setWindowParameters() {
 		// window size, titile and initial background
-		windowWidth = 800;
-		windowHeight = 600;
+		windowWidth = 1700;
+		windowHeight = 1000;
 		windowTitle = "My Project";
 		initialBackgroundColor = {0.0f, 0.0f, 0.0f, 1.0f};
 		
@@ -122,9 +122,9 @@ class MyProject : public BaseProject {
 					
 					
 		UniformBufferObject ubo{};
-		ubo.model = glm::rotate(glm::mat4(1.0f),
-								time * glm::radians(90.0f),
-								glm::vec3(0.0f, 0.0f, 1.0f));
+	
+		ubo.model = glm::mat4(1.0f)*0.5f;
+								
 		ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f),
 							   glm::vec3(0.0f, 0.0f, 0.0f),
 							   glm::vec3(0.0f, 0.0f, 1.0f));
